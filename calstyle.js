2f1fccd64309ee45f2de978a2450db1428b18fa4
Shaila Ahmed <shaila.ahmed1605@gmail.com>
2025-09-02 13:07:15 +0530

    calstyle.js

--- calstyle.js
+++ calstyle.js
@@ -32,46 +32,7 @@ buttons.forEach(element => {
             if (inputbox.value === '0' && btnText !== '.') {
                 string = btnText; // replace starting zero
             }
-                else if (btnText === "π") {
-                string += Math.PI;
-                display.value = string;
-            }
-            else if (btnText === "e") {
-                string += Math.E;
-                display.value = string;
-            }
-            else if (btnText === "sin") {
-                string = Math.sin(eval(string) * Math.PI / 180);
-                display.value = string;
-            }
-            else if (btnText === "cos") {
-                string = Math.cos(eval(string) * Math.PI / 180);
-                display.value = string;
-            }
-            else if (btnText === "tan") {
-                string = Math.tan(eval(string) * Math.PI / 180);
-                display.value = string;
-            }
-            else if (btnText === "log") {
-                string = Math.log10(eval(string));
-                display.value = string;
-            }
-            else if (btnText === "ln") {
-                string = Math.log(eval(string));
-                display.value = string;
-            }
-            else if (btnText === "√") {
-                string = Math.sqrt(eval(string));
-                display.value = string;
-            }
-            else if (btnText === "x²") {
-                string = Math.pow(eval(string), 2);
-                display.value = string;
-            }
-            else if (btnText === "x³") {
-                string = Math.pow(eval(string), 3);
-                display.value = string;
-            }
+                
             else {
                 string += btnText;
             }
@@ -79,4 +40,5 @@ buttons.forEach(element => {
         }
     });
 });
+
 
